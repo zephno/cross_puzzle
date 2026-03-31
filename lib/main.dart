a42cffcd0345849eee7840684944a874ae01ab96
zephno <paololacandula@gmail.com>
2026-03-31 12:05:07 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'choose_mode_page.dart';
 import 'profile.dart';
-import 'screens/daily_puzzle_screen.dart'; // ← new import
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
+  
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
